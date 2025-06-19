628268cdb9533fe91edf6b74d5bfc5b44f78d035
fizzashahbaz <fizzashahbaz20@gmail.com>
2025-06-19 07:50:22 +0500

    sidebar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      HELLO
-    </div>
+    <>
+     <Sidebar/>
+    </>
   )
 }
 
